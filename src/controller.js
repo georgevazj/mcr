32b3f3ebf5744266d05e2463358786c721ee6b0d
Jorge <georgevazj@gmail.com>
2017-01-22 21:30:13 +0100

    Modificacion de idiomas

--- src/controller.js
+++ src/controller.js
@@ -1,10 +1,25 @@
 'use strict';
 var app = angular.module("mcr")
-app.controller("HeaderController", headerCtrl);
+app.controller("MainController", mainCtrl);
 
-function headerCtrl($http) {
+function mainCtrl($http) {
 	var vm = this;
-	this.title = "MCR";
-	this.subtitle = "Artist advisor";
-	this.buttonLink = "Find Out More";
+	
+	vm.title = "MCR";
+	vm.subtitle = "Artist advisor";
+
+	vm.switchLanguage = function(lang){
+		if (lang=="en"){
+			vm.subtitle = "Artist advisor";
+			vm.buttonLink = "Find Out More";
+		}
+		else if(lang == "rs") {
+			vm.subtitle = "художник советник";
+			vm.buttonLink = "Узнать больше";
+		}
+		else if(lang == "ch"){
+			vm.subtitle = "艺术家顾问";
+			vm.buttonLink = "了解更多";
+		}
+	}
 }
